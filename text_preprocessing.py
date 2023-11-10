33ba2b50238fb649a69c015b826d3bb2dc687736
DESKTOP-RGI068D\acastro <auradeeconcepcion@gmail.com>
2023-11-10 18:32:22 -0500

    #62 rolled back changes on stopwords, i.e. separated it from spacy lemma #42 added a flag to turn off webscraping for slangs, use the pickle file instead

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -24,6 +24,7 @@ Output: "Hello World! This is an email "
 import spacy
 import re
 import urllib3
+import utils
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -65,27 +66,53 @@ def replace_nonascii_characters(text):
     
 def get_emojis():
     emoji_dict = KeywordProcessor()
-    for k,v in EMOTICONS_EMO.items():
-        words = re.split(r',| or ', v.lower())
-        # get only the first word. remove "smiley" word.
-        word = words[0].replace(" smiley", "")
-        emoji_dict.add_keyword(k, words[0])
+    try:
+        for k,v in EMOTICONS_EMO.items():
+            words = re.split(r',| or ', v.lower())
+            # get only the first element. remove "smiley" word.
+            word = words[0].replace(" smiley", "")
+            # put in a dictionary
+            emoji_dict.add_keyword(k, words[0])
 
-    # additional emojis
-    emoji_dict.add_keyword("<3", "heart")
-    emoji_dict.add_keyword("</3", "heartbroken")
+        # additional emojis
+        emoji_dict.add_keyword("<3", "heart")
+        emoji_dict.add_keyword("</3", "heartbroken")
+    except Exception as err:
+        print(f"ERROR: {err}")
 
     return emoji_dict
+
+
+def get_slang_words(webscraped=False):
+    # TODO: Put in a config file
+    filename = './dictionary/slang_words_dictionary.pkl'
+    
+    slang_word_dict = KeywordProcessor()
+    try:
+        if webscraped:
+            # build slang words dictionary by webscraping
+            slang_word_dict = webscrape_slang_words()
+            # update the existing pickle file
+            utils.save_pickle_file(slang_word_dict, filename)
+        else:
+            # open a pickle file
+            slang_word_dict = utils.open_pickle_file(filename)           
+    except Exception as err:
+        print(f"ERROR: {err}")
+        print(f"Importing slang words dictionary from file..")
+        # open a pickle file
+        slang_word_dict = utils.open_pickle_file(filename)
+    
+    return slang_word_dict
 
 
 def webscrape_slang_words():
     http = urllib3.PoolManager()
     slang_word_dict = KeywordProcessor()
-
     try:
-        # TODO: need to save the content in a file
         for i in range(97,123):
-            page = http.request('GET', 'https://www.noslang.com/dictionary/'+chr(i))
+            # site where the slang words will be scraped
+            page = http.request('GET', 'https://www.noslang.com/dictioary/'+chr(i))
             soup = BeautifulSoup(page.data, 'html.parser')
 
             for elem in soup.findAll('div', class_="dictonary-word"): 
@@ -93,6 +120,7 @@ def webscrape_slang_words():
 
                 key = slang_word[0 : slang_word.rfind(":")-1]
                 value = elem.find('dd', class_="dictonary-replacement").get_text()
+                # put in a dictionary
                 slang_word_dict.add_keyword(key.lower(), value.lower())
     except Exception as err:
         print(f"ERROR: {err}")
@@ -100,15 +128,12 @@ def webscrape_slang_words():
     return slang_word_dict
 
 
-def remove_stopwords(text):
-    # Initialize the English stop words list
-    stop_words = set(stopwords.words('english'))
-    # Tokenize the text
+def remove_stopwords(text, list_of_stopwords):  
+    # remove stop words from the tokenized text
     tokens = word_tokenize(text)
-    # Remove stop words from the tokenized text
-    filtered_tokens = [token for token in tokens if token not in stop_words and "-" not in token and token.isalpha()]
-    # Join the non-stopwords back into a string
+    filtered_tokens = [token for token in tokens if token not in list_of_stopwords and "-" not in token and token.isalpha()]
     filtered_text = " ".join(filtered_tokens)
+
     return filtered_text
 
 
@@ -118,6 +143,7 @@ def lemmatize_text(texts):
     nlp = spacy.load("en_core_web_sm")
     
     list_of_lemmatized_texts = []
+    # customize spacy pipeline to apply two processors to a batch of 2000 records, and to exclude 'ner', 'parser' and 'textcat'
     for doc in nlp.pipe(texts, n_process=2, batch_size=2000, disable=['ner', 'parser', 'textcat']):
         lemmatized_texts = []
         for token in doc:
