41c5a253725bf4fe0326441ebe56b8ccabb96f97
RochanMehta <rochan.mehta1993@gmail.com>
2023-11-24 10:27:34 +0000

    displayed 2 reports in 1 row

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 # app.py
 import streamlit as st
 import pandas as pd
+import altair as alt
 import numpy as np
 import datetime
-import altair as alt
 
 def generate_report(filtered_df):
-    # Calculate some metrics using the numerical columns
+    # Calculate some hypothetical metrics using the numerical columns
     total_salary = filtered_df['Salary'].sum()
     average_work_hours = filtered_df['Work_Hours'].mean()
 
@@ -18,7 +18,7 @@ def generate_report(filtered_df):
         tooltip=['Department', 'Salary']
     ).properties(
         title='Salary by Department (colored by Gender)',
-        width=500
+        width=300
     )
 
     # Create a line chart
@@ -29,7 +29,7 @@ def generate_report(filtered_df):
         tooltip=['Department', 'Work_Hours']
     ).properties(
         title='Work Hours by Department (colored by Gender)',
-        width=500
+        width=300
     )
 
     # Create a bar chart for Age Distribution by Gender
@@ -39,7 +39,7 @@ def generate_report(filtered_df):
         color='Gender:N'
     ).properties(
         title='Age Distribution by Gender',
-        width=500
+        width=300
     )
 
     # Create a bar chart for Salary Distribution by Department
@@ -49,25 +49,31 @@ def generate_report(filtered_df):
         color='Gender:N'
     ).properties(
         title='Salary Distribution by Department',
-        width=500
+        width=300
     )
 
-    # Display the charts
+    # Display the charts in a single row
     st.header("Report:")
     st.write(f"Total Salary: {total_salary}")
     st.write(f"Average Work Hours: {average_work_hours}")
 
-    st.subheader("Bar Chart:")
-    st.altair_chart(bar_chart)
+    charts_row1, charts_row2 = st.columns(2)
 
-    st.subheader("Line Chart:")
-    st.altair_chart(line_chart)
+    with charts_row1:
+        st.subheader("Bar Chart:")
+        st.altair_chart(bar_chart)
 
-    st.subheader("Age Distribution by Gender:")
-    st.altair_chart(age_distribution_chart)
+    with charts_row1:
+        st.subheader("Line Chart:")
+        st.altair_chart(line_chart)
 
-    st.subheader("Salary Distribution by Department:")
-    st.altair_chart(salary_distribution_chart)
+    with charts_row2:
+        st.subheader("Age Distribution by Gender:")
+        st.altair_chart(age_distribution_chart)
+
+    with charts_row2:
+        st.subheader("Salary Distribution by Department:")
+        st.altair_chart(salary_distribution_chart)
 
 def main():
     st.title("User Input Streamlit App")
@@ -86,11 +92,11 @@ def main():
     # Input 3: Date selection for Joining Date
     input_joining_date = st.date_input("Input 3: Select Joining Date", datetime.date.today())
 
-    # Convert input_joining_date to Pandas Timestamp
-    input_joining_date = pd.to_datetime(input_joining_date)
-
     # Button to trigger the display
     if st.button("Filter and Generate Report"):
+        # Convert input_joining_date to Pandas Timestamp
+        input_joining_date = pd.to_datetime(input_joining_date)
+
         # Filter DataFrame based on selected values
         filtered_df = df[
             (df['Department'].str.lower() == input_department) &
