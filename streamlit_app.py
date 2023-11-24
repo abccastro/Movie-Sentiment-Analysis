2dfa834a0950480b9410ac5e186003134a2cb57e
RochanMehta <rochan.mehta1993@gmail.com>
2023-11-24 09:45:52 +0000

    added 3 types of input. 1 textbox for text input 2 dropdown input 3 date input

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,27 @@
 # app.py
 import streamlit as st
+import datetime
 
 def main():
-    st.title("Streamlit App with Dropdown")
+    st.title("User Input Streamlit App")
 
-    # Add a dropdown
-    selected_option = st.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
+    # Input 1: Textbox
+    input_text = st.text_input("Input 1: Enter some text")
 
-    st.write(f"You selected: {selected_option}")
+    # Input 2: Dropdown
+    options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
+    input_dropdown = st.selectbox("Input 2: Select an option", options)
+
+    # Input 3: Date selection
+    input_date = st.date_input("Input 3: Select a date", datetime.date.today())
+
+    # Button to trigger the display
+    if st.button("Show Selected Values"):
+        # Display selected values
+        st.header("Selected Values:")
+        st.subheader(f"Input 1 (Text): {input_text}")
+        st.subheader(f"Input 2 (Dropdown): {input_dropdown}")
+        st.subheader(f"Input 3 (Date): {input_date}")
 
 if __name__ == "__main__":
     main()
