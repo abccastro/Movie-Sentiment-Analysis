e1175768bba36286682064416d8b901e56a8f52c
DESKTOP-RGI068D\acastro <auradeeconcepcion@gmail.com>
2023-12-05 22:36:16 -0500

    minor changes

--- app_final.py
+++ app_final.py
@@ -91,10 +91,11 @@ def compare_with_existing_embeddings(all_chunks, input_val, sdate="", edate=""):
         if cos_sim > threshold:
             metadata = {
                 'Review': review_text,
+                'Review Date': row['date'],
                 'cosine_similarity': cos_sim,
-                'sentiment': row['review_sentiment'],
-                'titles': row['title'],
-                'date': row['movie_year'],
+                'Sentiment': row['review_sentiment'],
+                'Movie Title': row['title'],
+                'Movie Year': row['movie_year'],
             }
             matching_reviews.append(metadata)
 
@@ -121,129 +122,10 @@ def filter_by_movie_title(df, input_title,sdate="",edate=""):
     # Filter the DataFrame based on the lowercase and stripped input movie title
     filtered_df = df[df['movie_title_lower'] == input_title_lower]
 
-    # # Check if both start date and end date are provided
-    # if sdate and edate:
-    #     # Convert dates to numbers (assuming they are strings)
-    #     sdate = int(sdate)
-    #     edate = int(edate)
-
-    #     # Ensure start date is not higher than end date
-    #     if sdate > edate:
-    #         st.warning("Start date should not be higher than end date.")
-    #         return pd.DataFrame()
-        
-    #     filtered_df['release_year'] = pd.to_numeric(filtered_df['release_year'], errors='coerce')
-
-    #     # Filter the DataFrame based on the date range
-    #     filtered_df = filtered_df[(filtered_df['release_year'] >= sdate) & (filtered_df['release_year'] <= edate)]
-
     # Drop the temporary lowercase column
     filtered_df = filtered_df.drop(columns=['movie_title_lower'])
 
     return filtered_df
-
-
-###-------> FILTER CSV WITH MOVIE TITLES
-def filter_titles(csv_path, movie_titles):
-    # Read the CSV file
-    df = pd.read_csv(csv_path)
-
-    # Initialize an empty list to store matching reviews
-    matching_reviews = []
-
-    # Iterate through the reviews
-    for i, row in df.iterrows():
-        # Extract the movie title and convert to lowercase
-        title = row['title']
-
-        # Check if any word in the title (lowercase and stripped) is present in the list of movie titles
-        if any(word.strip() in title for word in movie_titles):
-            metadata = {
-                'imdb_id': row['imdb_id'],
-                'title': row['title'],
-                'release_year': row['release_year'],
-                'genre': row['genre'],
-                'production_companies': row['production_companies'],
-                'cast': row['cast'],
-                'director': row['director'],
-                'budget': row['budget'],
-                'revenue': row['revenue'],
-                'runtime': row['runtime'],
-                'vote_average': row['vote_average'],
-                'movie_sentiment': row['movie_sentiment']
-            }
-            matching_reviews.append(metadata)
-
-    # Create a DataFrame with the matching reviews
-    df_result = pd.DataFrame(matching_reviews)
-
-    return df_result
-
-@st.cache_data
-###-------> FILTER CSV WITH MOVIE TITLES
-def filter_titles_by_id(csv_path, imdb_id):
-    # Read the CSV file
-    df = pd.read_csv(csv_path)
-
-    # Initialize an empty list to store matching reviews
-    matching_reviews = []
-
-    # Iterate through the reviews
-    for i, row in df.iterrows():
-        # Check if the 'imdb_id' matches the specified IMDb ID
-        if str(row['imdb_id']).strip() == str(imdb_id).strip():
-            metadata = {
-                'imdb_id': row['imdb_id'],
-                'title': row['title'],
-                'release_year': row['release_year'],
-                'genre': row['genre'],
-                'production_companies': row['production_companies'],
-                'cast': row['cast'],
-                'director': row['director'],
-                'budget': row['budget'],
-                'revenue': row['revenue'],
-                'runtime': row['runtime'],
-                'vote_average': row['vote_average'],
-                'movie_sentiment': row['movie_sentiment']
-            }
-            matching_reviews.append(metadata)
-
-    # Create a DataFrame with the matching reviews
-    df_result = pd.DataFrame(matching_reviews)
-
-    return df_result
-
-def filter_cast(csv_path, movie_titles):
-    # Read the CSV file
-    df = pd.read_csv(csv_path)
-
-    # Initialize a dictionary to store cast lists for each movie
-    movie_casts = {title: set() for title in movie_titles}
-
-    # Iterate through the reviews
-    for i, row in df.iterrows():
-        # Extract the movie title and convert to lowercase
-        title = row['title']
-
-        # Check if the current movie is in the specified list
-        if title in movie_titles:
-            # Extract the cast list and add it to the set for the current movie
-            cast_list = eval(row['cast']) if pd.notna(row['cast']) else []
-            movie_casts[title].update(cast_list)
-
-    # Find the common cast members
-    common_cast_members = set.intersection(*movie_casts.values())
-
-    # Create a dictionary with the common cast members
-    result_dict = {
-        'Titles': movie_titles,
-        'Common_Cast_Members': list(common_cast_members)
-    }
-
-    # Convert the dictionary to a DataFrame
-    result_df = pd.DataFrame([result_dict])
-
-    return result_df
 
 
 def filter_top10_reviews(df_result, sentiment_type):
@@ -256,9 +138,9 @@ def filter_top10_reviews(df_result, sentiment_type):
         sentiments = ['positive']
 
     # # Check if the Drame is not empty and contains the required columns
-    if not df_result.empty and 'sentiment' in df_result.columns and 'cosine_similarity' in df_result.columns:
+    if not df_result.empty and 'Sentiment' in df_result.columns and 'cosine_similarity' in df_result.columns:
     # Lowercase theataF 'sentiment' column and create a temporary lowercase column
-        df_result['sentiment_lower'] = df_result['sentiment'].str.lower()
+        df_result['sentiment_lower'] = df_result['Sentiment'].str.lower()
 
         # Filter the DataFrame based on the lowercase 'sentiment' column
         df_result = df_result[df_result['sentiment_lower'].isin(sentiments)]
@@ -268,34 +150,6 @@ def filter_top10_reviews(df_result, sentiment_type):
 
         # Sort the DataFrame by Cosine Similarity in descending order
         df_result = df_result.sort_values(by='cosine_similarity', ascending=False).head(10)
-
-        return df_result
-    else:
-        # Handle the case where the DataFrame is empty or missing required columns
-        return pd.DataFrame()
-
-
-def filter_top10movie_reviews(df_result, sentiment_type):
-
-    if sentiment_type == Sentiment.Negative.value:
-        sentiments = ['negative']
-    elif sentiment_type == Sentiment.Neutral.value:
-        sentiments = ['neutral']
-    elif sentiment_type == Sentiment.Positive.value:
-        sentiments = ['positive']
-
-    if not df_result.empty and 'review_sentiment' in df_result.columns:
-    # Lowercase the 'sentiment' column and create a temporary lowercase column
-        df_result['sentiment_lower'] = df_result['review_sentiment'].str.lower()
-
-        # Filter the DataFrame based on the lowercase 'sentiment' column
-        df_result = df_result[df_result['sentiment_lower'].isin(sentiments)]
-
-        # Drop the temporary lowercase column
-        df_result = df_result.drop(columns=['sentiment_lower'])
-
-        # Sort the DataFrame by Cosine Similarity in descending order
-        df_result = df_result.head(10)
 
         return df_result
     else:
@@ -356,8 +210,8 @@ def conduct_text_preprocessing(text, set_n=1):
             text = tp.remove_stopwords(text, list_of_stopwords)
 
     except Exception as err:
-        print(f"ERROR: {err}")
-        print(f"Input Text: {text}")
+        st.code(f"ERROR: {err}")
+        st.code(f"Input Text: {text}")
 
     return text
 
@@ -394,7 +248,7 @@ def generate_sentiment(df):
         predictions  = sentiment_dict.get(predictions[0])
 
     except Exception as err:
-        print(f"ERROR: {err}")
+        st.code(f"ERROR: {err}")
 
     return predictions
 
@@ -416,8 +270,9 @@ def get_top_closest_movie_names(movie_name, df, top_n=10):
 
         top_closest_match_idx = np.argsort(similarity_scores[0])[::-1][:top_n]
         top_closest_match_name = movie_names.loc[top_closest_match_idx.tolist()].values
+
     except Exception as err:
-        print(f"ERROR: {err}")
+        st.code(f"ERROR: {err}")
 
     return top_closest_match_name
 
@@ -447,8 +302,7 @@ def get_movie_recommendation(movie_name, top_n=10):
                 vg_dist_idx_df = pd.concat([pd.DataFrame(vg_indices[idx]), pd.DataFrame(vg_distances[idx])], axis=1)
                 vg_combined_dist_idx_df = pd.concat([vg_combined_dist_idx_df, vg_dist_idx_df])
 
-            vg_combined_dist_idx_df = vg_combined_dist_idx_df.set_axis(['Index', 'Distance'], axis=1)
-            # vg_combined_dist_idx_df = vg_combined_dist_idx_df.set_axis(['Index', 'Distance'], axis=1, inplace=False)
+            vg_combined_dist_idx_df = vg_combined_dist_idx_df.set_axis(['Index', 'Distance'], axis=1, inplace=False)
             vg_combined_dist_idx_df = vg_combined_dist_idx_df.reset_index(drop=True)
             vg_combined_dist_idx_df = vg_combined_dist_idx_df.sort_values(by='Distance', ascending=True)
 
@@ -464,7 +318,7 @@ def get_movie_recommendation(movie_name, top_n=10):
             recommended_movie_list = recommended_movie_list.reset_index(drop=True)
 
     except Exception as err:
-        print(f"ERROR: {err}")
+        st.code(f"ERROR: {err}")
 
     return recommended_movie_list,1
 
@@ -601,8 +455,9 @@ def main():
         st.header("Get Movie Review Sentiment")
         review_movie_text = st.selectbox("Select Movie Title", title_movie, key="review_movie_text")
         review_text = st.text_input("Enter Review:")
-
         show_review = st.button("Find Sentiment", on_click=callback3)
+
+        st.divider()
 
         ########## MOVIE ASPECTED-BASED SENTIMENT ##########
         sdate = things['movie_year'].unique().tolist()
@@ -614,22 +469,23 @@ def main():
         query_text = st.text_input("Enter Query:")
         start_date = st.selectbox("Select Start Date", sdate, key="start_date")
         end_date = st.selectbox("Select End Date", edate, key="end_date")
-
         show_match = st.button("Find Reviews", on_click=callback, key="show_match")
+
+        st.divider()
 
         ########## MOVIE REVIEWS ##########
         st.header("Get Movie Reviews")
         query_movie = st.selectbox("Select Movie Title", title_movie, key="query_movie")
-        # start_date_movie = st.selectbox("Select Start Date", sdate, key="start_date_movie")
-        # end_date_movie = st.selectbox("Select End Date", edate, key="end_date_movie")
-
         show_movie = st.button("Find Reviews", on_click=callback2, key="show_movie")
+
+        st.divider()
 
         ########## MOVIE RECOMMENDATION ##########
         st.header("Get Movie Recommendation")
         movie_rec = st.text_input("Enter Movie Title: ")
         show_recommendations = st.button("Find Recommendations", on_click=callback4)
 
+        st.divider()
 
     ########################################################
     #  DISPLAY PANEL: DASHBOARD
@@ -645,10 +501,10 @@ def main():
             st.subheader(f"Movie Information")
             st.dataframe(movie_info, hide_index=True)
 
-            st.subheader(f"Top 10 Recommended Movies for '{movie_info.iloc[0]['title']}'")
+            st.subheader(f"Top 10 Recommended Movies for :blue[{movie_info.iloc[0]['title']}]")
             st.dataframe(movie_list, hide_index=True)
         else:
-            st.write(f"The movie'{movie_rec}' doesn't exist in the records.")
+            st.subheader(f"The movie :blue[{movie_rec}] doesn't exist in the records.")
             st.write("You may want to try the movies which are the closest match to the input.")
             st.dataframe(result_df_2_recommend, hide_index=True)
 
@@ -656,12 +512,19 @@ def main():
     if st.session_state.show_review_state:
         
         input_review = review_text.strip()
-        st.write(f"**Review:** {review_text}")
+        st.subheader(f"Review: :blue[{review_text}]")
 
         result_df_2_review = process_movie_review(st.session_state.all_chunks, review_movie_text, review_text)
         
         if not result_df_2_review.empty:    
-            st.dataframe(result_df_2_review[["Review","sentiment"]].head(1), hide_index=True)
+            sentiment_final = result_df_2_review['sentiment'].str.split(" ")
+            st.write(f"{sentiment_final}")
+            if str(sentiment_final).lower() == "negative":
+                st.header(f"This review gave a :red[Negative] sentiment.")
+            elif str(sentiment_final).lower() == "positive":
+                st.header(f" This review gave a :green[Positive] sentiment. ")
+            else:
+                st.header(f"This review gave a :grey[Neutral] sentiment.")
         else:
             st.write(f"The movie '{review_movie_text}' doesn't exist in the records.")
 
@@ -669,19 +532,14 @@ def main():
     if st.session_state.show_match_state:
 
         input_review = query_text.strip()
-        result_df_2 = compare_with_existing_embeddings(st.session_state.all_chunks ,input_review,start_date,end_date)
-        st.dataframe(result_df_2, hide_index=True)
-        # Add a download button for top 10 positive reviews
-        csv_export_button_pos = st.download_button(
-            label=f"All reviews relating to {input_review}",
-            data=result_df_2.to_csv().encode(),
-            file_name=f'{input_review}_reviews.csv',
-            mime='text/csv'
-        )
+        result_df_2 = compare_with_existing_embeddings(st.session_state.all_chunks, input_review,start_date,end_date)
+
+        st.subheader(f"Movie Reviews related to '{input_review}'")
+        st.dataframe(result_df_2.drop(columns=['cosine_similarity'], axis=1), hide_index=True)
 
         if not result_df_2.empty:
-        # Display a bar chart based on sentiment counts
-            chart_data = pd.DataFrame(result_df_2['sentiment'].value_counts()).reset_index()
+            # Display a bar chart based on sentiment counts
+            chart_data = pd.DataFrame(result_df_2['Sentiment'].value_counts()).reset_index()
             chart_data.columns = ['Sentiment', 'Count']
 
             st.subheader("Sentiment Distribution")
@@ -695,18 +553,18 @@ def main():
 
             # Filter and display top 10 positive reviews
             result_df_tp_10_pos = filter_top10_reviews(result_df_2, Sentiment.Positive.value)
-            st.subheader(f"Top 10 (Positive) Reviews around: {input_review}")
-            st.dataframe(result_df_tp_10_pos)
+            st.subheader(f"Top 10 :green[Positive] Reviews related to '{input_review}'")
+            st.dataframe(result_df_tp_10_pos.drop(columns=['cosine_similarity', 'Sentiment'], axis=1), hide_index=True)
 
             # Filter and display top 10 negative reviews
             result_df_tp_10_neg = filter_top10_reviews(result_df_2, Sentiment.Negative.value)
-            st.subheader(f"Top 10 (Negative) Reviews around: {input_review}")
-            st.dataframe(result_df_tp_10_neg)
+            st.subheader(f"Top 10 :red[Negative] Reviews related to '{input_review}'")
+            st.dataframe(result_df_tp_10_neg.drop(columns=['cosine_similarity', 'Sentiment'], axis=1), hide_index=True)
 
             # Filter and display top 10 negative reviews
             result_df_tp_10_neu = filter_top10_reviews(result_df_2, Sentiment.Neutral.value)
-            st.subheader(f"Top 10 (Neutral) Reviews around: {input_review}")
-            st.dataframe(result_df_tp_10_neu)
+            st.subheader(f"Top 10 :grey[Neutral] Reviews related to '{input_review}'")
+            st.dataframe(result_df_tp_10_neu.drop(columns=['cosine_similarity', 'Sentiment'], axis=1), hide_index=True)
 
         else:
             st.dataframe(pd.DataFrame())
@@ -716,18 +574,15 @@ def main():
 
         input_review_movie = query_movie.strip()
         result_df_movie = filter_by_movie_title(st.session_state.all_chunks, input_review_movie)
+
+        new_column_names = {'date': 'Review Date', 'review_sentiment': 'Sentiment'}
+        result_df_movie.rename(columns=new_column_names, inplace=True)
+
         st.subheader(f"Movie Reviews on '{input_review_movie}'")
-        st.dataframe(result_df_movie[['Review', 'date', 'review_sentiment']], hide_index=True)
-        # Add a download button for top 10 positive reviews
-        csv_export_button_pos = st.download_button(
-            label=f"All reviews relating to {input_review_movie}",
-            data=result_df_movie.to_csv().encode(),
-            file_name=f'{input_review_movie}_reviews.csv',
-            mime='text/csv'
-        )
+        st.dataframe(result_df_movie[['Review', 'Review Date', 'Sentiment']], hide_index=True)
 
         # Display a bar chart based on sentiment counts
-        chart_data = pd.DataFrame(result_df_movie['review_sentiment'].value_counts()).reset_index()
+        chart_data = pd.DataFrame(result_df_movie['Sentiment'].value_counts()).reset_index()
         chart_data.columns = ['Sentiment', 'Count']
 
         st.subheader("Sentiment Distribution")
@@ -738,22 +593,7 @@ def main():
         ).properties(width=800, height=600)
 
         st.altair_chart(c)
-
-        # Filter and display top 10 positive reviews
-        result_df_tp_10movie_pos = filter_top10movie_reviews(result_df_movie, Sentiment.Positive.value)
-        st.subheader(f"Top 10 (Positive) Reviews")
-        st.dataframe(result_df_tp_10movie_pos[['Review', 'date']], hide_index=True)
-
-        # Filter and display top 10 negative reviews
-        result_df_tp_10movie_neg = filter_top10movie_reviews(result_df_movie, Sentiment.Negative.value)
-        st.subheader(f"Top 10 (Negative) Reviews")
-        st.dataframe(result_df_tp_10movie_neg[['Review', 'date']], hide_index=True)
-
-        # Filter and display top 10 negative reviews
-        result_df_tp_10movie_neu = filter_top10movie_reviews(result_df_movie, Sentiment.Neutral.value)
-        st.subheader(f"Top 10 (Neutral) Reviews")
-        st.dataframe(result_df_tp_10movie_neu[['Review', 'date']], hide_index=True)
-
+        
 
 if __name__ == "__main__":
     main()
